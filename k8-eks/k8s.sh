89b7e7c7be650f5b4527f62af92053885b09ad78
msivakumarreddy <info@joindevops.com>
2025-11-25 15:25:05 +0530

    msg

---
+++ k8-eks/k8s.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+growpart /dev/nvme0n1 4
+lvextend -L +20G /dev/RootVG/rootVol
+lvextend -L +10G /dev/RootVG/varVol
+
+xfs_growfs /
+xfs_growfs /var
+
+# Set up kubectl and eksctl 
+eksctl install 
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+ curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | gre               p $PLATFORM | sha256sum --check
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+eksctl version 
+kubectl install 
+
+# Set up kubectl and eksctl 
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.5/2025-11-13/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+kubectl version
