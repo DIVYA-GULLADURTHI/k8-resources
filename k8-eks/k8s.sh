15a76f8067ae06b1a7f3d4d6faec55df783f325b
msivakumarreddy <info@joindevops.com>
2025-12-25 13:27:23 +0530

    kubernates

--- k8-eks/k8s.sh
+++ k8-eks/k8s.sh
@@ -24,3 +24,5 @@ curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.5/2025-11-13/bin/linu
 chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
 kubectl version
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens 
